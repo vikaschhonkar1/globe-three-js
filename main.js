4da81a702ee66c0dca65753ac7cfc0f1878e167e
Vikas Chhonkar <vikaschhonkar1@gmail.com>
2023-08-31 11:47:46 +0530

    Added basic components of three js scenes

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-console.log("Hello Three JS")
+console.log("Hello Three JS");
+
+// main components of three js scenes are :
+// camera
+// light
+// renderer
+
+function main() {
+  // scene
+  const scene = new THREE.Scene();
+
+  // renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector("#globe"),
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // camera
+  const camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  const render = () => {
+    renderer.render(scene, camera);
+  };
+  render();
+}
+
+window.onload = main;
